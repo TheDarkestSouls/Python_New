05a1011cca43e1004e95246a00851f40a8f28e3e
NikitaP <unntiss@bk.ru>
2022-10-31 18:38:14 +0300

    t1mod

--- Workshop/Sem1/task1.py
+++ Workshop/Sem1/task1.py
@@ -6,9 +6,17 @@
 # - 7 -> да
 # - 1 -> нет
 
-day = int(input())
-if day == 6 or day == 7:
+# day = int(input())
+# if day == 6 or day == 7:
+#     print("Yes")
+# else:
+#     print("No")
+
+weekend = int(input())
+if 5 < weekend < 8:
     print("Yes")
 else:
     print("No")
+
+    
 
