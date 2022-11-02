d50ae6202c62b181ffcaffc72efc9563822ab1f1
NikitaP <unntiss@bk.ru>
2022-11-02 11:39:35 +0300

    s2t1 is improved with comrehension list

--- Workshop/Sem2/task1.py
+++ Workshop/Sem2/task1.py
@@ -5,10 +5,16 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
+# number = input()
+# sum = 0
+# for i in number:
+#     if i == ',':
+#         continue
+#     sum += int(i)
+# print(sum)
+
 number = input()
-sum = 0
-for i in number:
-    if i == ',':
-        continue
-    sum += int(i)
-print(sum)
+number = [int(i) for i in number if i.isnumeric()]
+print(sum(number))
+
+
