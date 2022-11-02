7683f5fec42cc9ac026b59c2e8fd4ce264d6e9a3
NikitaP <unntiss@bk.ru>
2022-11-02 12:34:33 +0300

    s2t4 is modified

--- Workshop/Sem2/task4.py
+++ Workshop/Sem2/task4.py
@@ -3,18 +3,25 @@
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
 
-N = int(input())
-list = []
-result = int(1)
-for i in range(-N, N + 1):   # make list
-    if i == 0:
-        continue
-    list.append(i)
-print(list)
+# N = int(input())
+# list = []
+# result = int(1)
+# for i in range(-N, N + 1):   # make list
+#     if i == 0:
+#         continue
+#     list.append(i)
+# print(list)
 
+# path = 'file.txt'
+# data = open(path, 'r')
+# for num in data:             # multiple its elements with indices specified in file.txt
+#     result *= list[int(num)]
+#     print(list[int(num)])
+# print(result)
+
+N = int(input())
+leeest = [i for i in range(-N, N+1) if i != 0]
 path = 'file.txt'
 data = open(path, 'r')
-for num in data:             # multiple its elements with indices specified in file.txt
-    result *= list[int(num)]
-    print(list[int(num)])
-print(result)
+leeest = [leeest[i] for i in data]
+print(leeest)
