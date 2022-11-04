6dfedea3d68dd499fe96d2a8c8dcacc1d47c6cf8
NikitaP <unntiss@bk.ru>
2022-11-04 12:03:31 +0300

    s3t2 is improved

--- Workshop/Sem3/Task2.py
+++ Workshop/Sem3/Task2.py
@@ -8,19 +8,21 @@
 
 from operator import index
 import random
-list = []
-l = int(input("enter the length of a list: "))
-for i in range(1, l+1):
-    list.append(random.randint(1,9)) # list = [x, x, x.....x]
-print(list)
-print()
+# list = []
+# l = int(input("enter the length of a list: "))
+# for i in range(1, l+1):
+#     list.append(random.randint(1,9)) # list = [x, x, x.....x]
+# print(list)
+# print()
 
-LN = 1
-for i in range(0, len(list)):
-    if i < len(list)/2:
-        print(list[i] * list[-LN])
-        LN += 1
+# LN = 1
+# for i in range(0, len(list)):
+#     if i < len(list)/2:
+#         print(list[i] * list[-LN])
+#         LN += 1
 
+num_list = [random.randint(1, 9) for i in range(1, int(input("Enter the amount of symbols: "))+1)]
+print(num_list, " => ", [num_list[i]*num_list[-(i+1)] for i in range(int(len(num_list)) - int(len(num_list)/2))])
 
 
     
