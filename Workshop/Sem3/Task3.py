ab99b52504f55978dfe2ec3ed4640aba73f57ed6
NikitaP <unntiss@bk.ru>
2022-11-04 12:31:42 +0300

    s3t3 is improved

--- Workshop/Sem3/Task3.py
+++ Workshop/Sem3/Task3.py
@@ -8,13 +8,18 @@
 # list = [2.35, 1.02, 15.87, 9.23, 7.15] # My example
 
 list = [1.1, 1.2, 3.1, 5, 10.01]         # Sergey example
-max = list[0]%1
-min = list[1]%1
-for i in list:
-    if i%1 > max:
-        max = i%1
-for j in list:
-    if j%1 < min:
-        min = j%1
-print(max - min)
+# max = list[0]%1
+# min = list[1]%1
+# for i in list:
+#     if i%1 > max:
+#         max = i%1
+# for j in list:
+#     if j%1 < min:
+#         min = j%1
+# print(max - min)
+
+li = sorted([int(i%1*100)/100 for i in list])
+print(li[-1] - li[0])
+
+
 
