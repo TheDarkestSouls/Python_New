27e108b2acf409b456e457994188e466714aa81c
NikitaP <unntiss@bk.ru>
2022-11-23 20:06:23 +0300

    2

--- Workshop/Sem1/task2.1.py
+++ Workshop/Sem1/task2.1.py
@@ -1,2 +1,9 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
 # not (X or Y or Z) = not X and not Y and not Z
+
+for X in range(2):
+    for Y in range(2):
+        for Z in range(2):
+            if not (X or Y or Z):
+                print(not X and not Y and not Z)
