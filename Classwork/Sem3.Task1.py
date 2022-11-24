1d4a7f4def3f85b5773ac11015d5300833290780
NikitaP <unntiss@bk.ru>
2022-11-24 22:08:59 +0300

    3

--- Classwork/Sem3.Task1.py
+++ Classwork/Sem3.Task1.py
@@ -1,8 +1,11 @@
-s=input("enter H or T as many times as wanted: ")
-t=0
-while "T"*(t+1) in s:
-    t+=1
-if t!=0:
-    print(t)
-else:
-    print(0)
+count = 0
+
+str1 = input()
+str2 = input()
+
+for i in range(len(str1) - (len(str2) - 1)):
+    if str2 == str1[i:i+len(str2)]:
+        count += 1
+
+
+print(f"Вторая строка входит в первую {count} раз(а).")
