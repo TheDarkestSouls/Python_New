b4141efa5af0c51c8ede75ed9fc25f2d8a58a8e9
NikitaP <unntiss@bk.ru>
2022-11-25 15:04:34 +0300

    4mod

--- Workshop2/hw2/task4.py
+++ Workshop2/hw2/task4.py
@@ -9,7 +9,7 @@ for i in range(-N, N+1):
 print(great_list)
 
 el_1 = great_list[int(input('multiplicand is: '))]
-el_2 = great_list[int(input('multiplicand is: '))]
+el_2 = great_list[int(input('multiplier is: '))]
 
 print(f'product is: {el_1 * el_2}')
 
