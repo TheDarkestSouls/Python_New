0c7d351594b61d3917c852903a7a431509b0d3f0
NikitaP <unntiss@bk.ru>
2022-11-30 00:17:48 +0300

    5 is done

--- Workshop2/hw3/task5.py
+++ Workshop2/hw3/task5.py
@@ -10,16 +10,16 @@ def fib(n):
     elif n == -2:
         return -1
     else:
-        return fib(n-1) + fib(n-2)
+        return fib(n - 1) + fib(n - 2)
 
 k = int(input())
 
 list1 = []
 list2 = []
-for e in range(1, k+1):
+for e in range(1, k + 1):
     list1.append(fib(e))
-for f in range(0, k+1):
-    if not f%2:
+for f in range(0, k + 1):
+    if not f % 2:
         list2.append(fib(f) * -1)
     else:
         list2.append(fib(f))
