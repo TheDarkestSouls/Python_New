d4ef674c078564d817cf7d2b1d4b449f31f19094
NikitaP <unntiss@bk.ru>
2022-11-29 20:24:30 +0300

    2 is done

--- Workshop2/hw3/task2.py
+++ Workshop2/hw3/task2.py
@@ -9,8 +9,8 @@
 def pair_product_list(list):
     new_list = []
     for i in range(len(list)):
-        if i <= len(list) / 2:
-            new_list.append(list[i+1] * list[-i])
+        if i < len(list) / 2:
+            new_list.append(list[i] * list[-(i + 1)])
         else:
             break
     return new_list
@@ -19,5 +19,5 @@ def pair_product_list(list):
 list1 = [2, 3, 4, 5, 6]
 list2 = [2, 3, 5, 6]
 
-pair_product_list(list1)
-pair_product_list(list2)
+print(pair_product_list(list1))
+print(pair_product_list(list2))
